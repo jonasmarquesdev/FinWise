a3fc4929a856bcad8933d6397c1ddff86f6b2da8
Jonas Marques <jonasmarquesuitec@gmail.com>
2025-01-25 20:34:18 -0300

    feat: add authentication status check

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { signIn, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 const LoginPage = () => {
+  const { status } = useSession();
+  if (status === "authenticated") {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
@@ -14,12 +22,12 @@ const LoginPage = () => {
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground mb-8">
+        <p className="mb-8 text-muted-foreground">
           A FinWise é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
+        <Button variant="outline" onClick={() => signIn()}>
           <LogInIcon className="mr-2" />
           Fazer login ou criar conta
         </Button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  const { status } = useSession();
+  if (status === "unauthenticated") {
+    redirect("/login");
+  }
   return (
     <div className="flex w-screen items-center justify-center">
       <h1 className="p-5 text-red-500">Home Page</h1>
