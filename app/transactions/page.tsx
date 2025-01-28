6aa5769417b2ee2d6b6b8f05535df1f526ae4b30
Jonas Marques <jonasmarquesuitec@gmail.com>
2025-01-27 21:40:43 -0300

    feat: ensure pages are correctly scrolled by the user

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -6,6 +6,7 @@ import NavBar from "../_components/navbar";
 import { authOptions } from "../_lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "../_components/ui/scroll-area";
 
 const TransactionsPage = async () => {
   const session = await getServerSession(authOptions);
@@ -20,15 +21,17 @@ const TransactionsPage = async () => {
   return (
     <>
       <NavBar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between p-6">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-        <DataTable
-          columns={transactionColumns}
-          data={JSON.parse(JSON.stringify(transactions))}
-        />
+        <ScrollArea>
+          <DataTable
+            columns={transactionColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
+        </ScrollArea>
       </div>
     </>
   );
