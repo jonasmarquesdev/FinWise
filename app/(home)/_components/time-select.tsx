dcaf9d40c2571b3fbf3f768a8452a5b06f7f6a11
Jonas Marques <jonasmarquesuitec@gmail.com>
2026-07-06 20:58:03 -0300

    feat: add new year option

--- app/(home)/_components/time-select.tsx
+++ app/(home)/_components/time-select.tsx
@@ -31,6 +31,7 @@ const YEAR_OPTIONS = [
   { value: "2024", label: "2024" },
   { value: "2025", label: "2025" },
   { value: "2026", label: "2026" },
+  { value: "2027", label: "2027" },
 ];
 
 const TimeSelect = () => {
